0d09bb290c71ff6c64e4f2b3c9c796d6f8d50f21
Natchkung <natchnon654321@gmail.com>
2024-04-12 18:35:32 +0700

    update copy your ip address

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -4,24 +4,29 @@
             <div class="flex justify-start items-center my-10">
                 <h1 class="font-bold text-2xl">DASHBOARD</h1>
             </div>
-            <div class="profile flex justify-start items-center">
-                <div class="profile-img w-[150px] h-[150px] bg-[#262626] rounded-full overflow-hidden">
-                    <img src="../../assets/noprofile.jpg"
-                        alt="profile" class="w-full h-full object-cover">
-                </div>
-                <div class="ml-10 flex flex-col gap-2 text-center justify-center items-start">
-                    <!-- <div class="text-xl font-semibold text-[#3690bd]"><span id="username">Natchkung</span></div> -->
-                    <div class="h-[40px] bg-[#262626] rounded-[5px] flex overflow-hidden">
-                        <div class="h-full bg-[#3d7fa1] flex justify-center items-center text-lg font-semibold px-5">
-                            <span>Username</span>
-                        </div>
-                        <div class="h-full flex justify-center items-center text-lg"><span id="username"
-                                class="px-5 text-center">{{ profile.username }}</span></div>
+            <div class="profile flex justify-between items-center">
+                <div class="flex justify-start items-center">
+                    <div class="profile-img w-[150px] h-[150px] bg-[#262626] rounded-full overflow-hidden">
+                        <img src="../../assets/noprofile.jpg" alt="profile" class="w-full h-full object-cover">
                     </div>
-                    <div class="text-[17px]"><span id="email">{{ profile.email }}</span></div>
-                    <div class="text-[17px] text-[#a3a3a3]">ID: <span ref="id">{{ profile.id }}</span><button
-                            @click="copy" class="ml-2 text-center text-[#3d7fa1] hover:text-sky-500"><i
-                                class="fa-solid fa-copy"></i></button></div>
+                    <div class="ml-10 flex flex-col gap-2 text-center justify-center items-start">
+                        <!-- <div class="text-xl font-semibold text-[#3690bd]"><span id="username">Natchkung</span></div> -->
+                        <div class="h-[40px] bg-[#262626] rounded-[5px] flex overflow-hidden">
+                            <div
+                                class="h-full bg-[#3d7fa1] flex justify-center items-center text-lg font-semibold px-5">
+                                <span>Username</span>
+                            </div>
+                            <div class="h-full flex justify-center items-center text-lg"><span id="username"
+                                    class="px-5 text-center">{{ profile.username }}</span></div>
+                        </div>
+                        <div class="text-[17px]"><span id="email">{{ profile.email }}</span></div>
+                        <div class="text-[17px] text-[#a3a3a3]">ID: <span ref="id">{{ profile.id }}</span><button
+                                @click="copy" class="ml-2 text-center text-[#3d7fa1] hover:text-sky-500"><i
+                                    class="fa-solid fa-copy"></i></button></div>
+                    </div>
+                </div>
+                <div>
+                    <button @click="ipaddress" class="bg-[#3d7fa1] text-white px-5 py-2 rounded-lg hover:bg-[#265e7a] duration-300"> Copy your IP address</button>
                 </div>
             </div>
             <div class="w-[75%] m-auto rounded-lg border border-[#555555] bg-[#262626] my-10 p-10 flex flex-col">
@@ -30,7 +35,7 @@
                     <div>
                         <span class="text-lg font-semibold">เปลี่ยน IP โปรดอ่าน!!</span>
                         <p>หากคุณได้เปลี่ยน IP สคริปต์นั้นไปแล้ว จะต้องรออีก <span class="text-rose-600">1
-                            วัน</span> ถึงจะสามารถปรับเปลี่ยน
+                                วัน</span> ถึงจะสามารถปรับเปลี่ยน
                             IP ของสคริปต์นั้นๆได้อีกครั้ง</p>
                     </div>
 
@@ -39,56 +44,60 @@
             <div class="license w-full flex justify-center items-center overflow-auto">
                 <div v-if="license.Enabled">
                     <table class="table-auto bg-[#262626] rounded-lg overflow-hidden">
-                    <thead>
-                        <tr class="bg-[#3d7fa1]">
-                            <th class="px-4 py-2 text-center">Name</th>
-                            <th class="px-4 py-2 text-center ">License</th>
-                            <th class="px-4 py-2 text-center">Active</th>
-                            <th class="px-4 py-2 text-center">IP Address</th>
-                            <th class="px-4 py-2 text-center">ตั้ง IP</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="data in license.data" :key="data.id">
-                            <td class="px-4 py-2 text-center">{{ data.nameScript }}</td>
-                            <td class="px-4 py-2 text-center flex gap-2 flex-wrap">
-                                <span v-if="data.show">{{ data.license }}</span>
-                                <span v-else class="blur">{{ data.license }}</span>
-                                <button @click="data.show = !data.show" class="text-[#3d7fa1] hover:text-sky-500"><i
-                                        class="fa-solid fa-eye"></i></button>
-                            </td>
-                            <td class="px-4 py-2 text-center">
-                                <Switch1 v-model="data.status" @click="LicenseActiveControl(data)"
-                                    :class="(data.status === 'active') ? 'bg-green-400' : 'bg-gray-200'"
-                                    class="relative inline-flex h-6 w-11 items-center rounded-full">
-                                    <span class="sr-only">Enable notifications</span>
-                                    <span :class="(data.status === 'inactive') ? 'translate-x-6' : 'translate-x-1'"
-                                        class="inline-block h-4 w-4 transform rounded-full bg-white transition" />
-                                </Switch1>
-                            </td>
-                            <td class="px-4 py-2 text-center">{{ data.ipaddress }}</td>
-                            <td class="px-4 py-2 text-center"><button @click="UiResetLicense(data)">
-                                    <i class="fa-solid fa-pen-to-square text-[#3d7fa1] text-lg hover:text-sky-500"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                        <thead>
+                            <tr class="bg-[#3d7fa1]">
+                                <th class="px-4 py-2 text-center">Name</th>
+                                <th class="px-4 py-2 text-center ">License</th>
+                                <th class="px-4 py-2 text-center">Active</th>
+                                <th class="px-4 py-2 text-center">IP Address</th>
+                                <th class="px-4 py-2 text-center">ตั้ง IP</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="data in license.data" :key="data.id">
+                                <td class="px-4 py-2 text-center">{{ data.nameScript }}</td>
+                                <td class="px-4 py-2 text-center flex gap-2 flex-wrap">
+                                    <span v-if="data.show">{{ data.license }}</span>
+                                    <span v-else class="blur">{{ data.license }}</span>
+                                    <button @click="data.show = !data.show" class="text-[#3d7fa1] hover:text-sky-500"><i
+                                            class="fa-solid fa-eye"></i></button>
+                                </td>
+                                <td class="px-4 py-2 text-center">
+                                    <Switch1 v-model="data.status" @click="LicenseActiveControl(data)"
+                                        :class="(data.status === 'active') ? 'bg-green-400' : 'bg-gray-200'"
+                                        class="relative inline-flex h-6 w-11 items-center rounded-full">
+                                        <span class="sr-only">Enable notifications</span>
+                                        <span :class="(data.status === 'inactive') ? 'translate-x-6' : 'translate-x-1'"
+                                            class="inline-block h-4 w-4 transform rounded-full bg-white transition" />
+                                    </Switch1>
+                                </td>
+                                <td class="px-4 py-2 text-center">{{ data.ipaddress }}</td>
+                                <td class="px-4 py-2 text-center"><button @click="UiResetLicense(data)">
+                                        <i
+                                            class="fa-solid fa-pen-to-square text-[#3d7fa1] text-lg hover:text-sky-500"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div v-else class="w-full h-[300px] flex justify-center items-center">
                     <div class="text-[#3d7fa1] text-2xl">You didn't own any resource. . .</div>
                 </div>
                 <div v-if="ResetLicenseData.show"
                     class="w-[500px] h-[100px] bg-[#202020] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg z-50">
-                        <div class="flex justify-start items-start w-full h-full flex-col">
-                            <div class="px-4 py-2 pt-4 font-semibold">IP Address</div>
-                            <div class="flex items-center justify-start">
-                                <input type="text" v-model="ResetLicenseData.data.newipaddress" placeholder="Enter new IP address" required
+                    <div class="flex justify-start items-start w-full h-full flex-col">
+                        <div class="px-4 py-2 pt-4 font-semibold">IP Address</div>
+                        <div class="flex items-center justify-start">
+                            <input type="text" v-model="ResetLicenseData.data.newipaddress"
+                                placeholder="Enter new IP address" required
                                 class="w-[400px] px-4 py-2 ml-2 text-white rounded-lg outline-none bg-transparent border-b-2 border-sky-500">
-                                <i class="fa-solid fa-floppy-disk hover:text-[#3d7fa1] duration-300 cursor-pointer ml-5" @click="ResetLicense()"></i>
-                                <i class="fa-solid fa-xmark hover:text-rose-700 duration-300 cursor-pointer ml-5" @click="ResetLicenseData.show = false"></i>
-                            </div>
+                            <i class="fa-solid fa-floppy-disk hover:text-[#3d7fa1] duration-300 cursor-pointer ml-5"
+                                @click="ResetLicense()"></i>
+                            <i class="fa-solid fa-xmark hover:text-rose-700 duration-300 cursor-pointer ml-5"
+                                @click="ResetLicenseData.show = false"></i>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -112,7 +121,7 @@ export default {
                 email: this.$cookies.get('ukb-data').email,
                 id: this.$cookies.get('ukb-data')['_id']
             },
-            ResetLicenseData:{
+            ResetLicenseData: {
                 show: false,
                 data: []
             },
@@ -136,9 +145,9 @@ export default {
             axios.post(api, {}, { withCredentials: true }).then(async (res) => {
                 if (res.status === 200) {
                     this.license.data = res.data
-                    if(this.license.data == 0){
+                    if (this.license.data == 0) {
                         this.license.Enabled = false
-                    }else{
+                    } else {
                         this.license.Enabled = true
                     }
                 }
@@ -193,7 +202,7 @@ export default {
                 }
             });
         },
-        UiResetLicense(data){
+        UiResetLicense(data) {
             this.ResetLicenseData.show = true
             this.ResetLicenseData.data = []
             this.ResetLicenseData.data.newipaddress = data.ipaddress
@@ -221,7 +230,7 @@ export default {
                 }
             }).catch((err) => {
                 console.log(err);
-                if(err.response.status == 400){
+                if (err.response.status == 400) {
                     toast(err.response.data, {
                         "theme": "dark",
                         "type": "error",
@@ -229,9 +238,24 @@ export default {
                         "dangerouslyHTMLString": true
                     })
                     setTimeout(() => {
-                    location.reload()
+                        location.reload()
                     }, 3000);
                 }
+            });
+        },
+        ipaddress() {
+            axios.get('https://api.ipify.org?format=json').then((res) => {
+                navigator.clipboard.writeText(res.data.ip);
+                toast(`คัดลอกไอพีของคุณสำเร็จ : ${res.data.ip}`, {
+                    "theme": "dark",
+                    "type": "success",
+                    "position": "top-center",
+                    "pauseOnHover": false,
+                    "autoClose": 1500,
+                    "dangerouslyHTMLString": true
+            })
+            }).catch((err) => {
+                console.log(err);
             });
         }
     },
